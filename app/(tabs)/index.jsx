9e18586a39594280184b87f0c9f7d88acb246ba0
nebyou <nebyouyohannes1@gmail.com>
2024-08-16 16:35:03 +0300

    updt

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,12 +1,18 @@
-import { Text,View,TextInput,StyleSheet } from "react-native"
+import { Text,View,TextInput,StyleSheet,Pressable } from "react-native"
 
 
 
 
 export default function mainApp(){
+  const pressed = () => {
+      alert('presseddddddd');
+  };
   return(
     <View style ={styles.container} >
       <TextInput style={styles.SearchBar}/>
+      <Pressable style={styles.srchBtn} onPress={pressed}>
+        <Text style={styles.text}>SEARCH</Text>
+      </Pressable>
     <Text>Helloo</Text>
     </View>
   )
@@ -22,6 +28,24 @@ const styles = StyleSheet.create({
       borderRadius: 15,
       backgroundColor: 'white',
       padding: 10,
-      
-    }
+    },
+    srchBtn:{
+    marginTop: 10,
+      alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+    },
+    text: {
+      fontSize: 16,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: 'white',
+    },
+
+
 });
